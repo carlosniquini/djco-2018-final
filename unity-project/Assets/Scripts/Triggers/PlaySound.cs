f82a0817b4929c0fa1292c9f97fdecd5516a1d91
Alessandro Antoniolli <ale_antonioli@hotmail.com>
2018-04-15 20:06:30 +0100

    Initial implementation of sounds triggers
    Initial implementation of sounds triggers

---
+++ unity-project/Assets/Scripts/Triggers/PlaySound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+
+	public AudioClip SoundToPlay;
+	public float volume;
+	AudioSource audio;
+	public bool alreadyPlayed = false;	
+
+	void Start(){
+		audio = GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(){
+		alreadyPlayed = true;
+		play();
+	}
+
+	void OnTriggerExit(){
+		alreadyPlayed = false;
+		audio.Stop ();
+	}
+
+	void play(){
+		audio.Play();
+		//audio.Loop();	
+		//audio.PlayOneShot (SoundToPlay, volume);
+	}
+}
