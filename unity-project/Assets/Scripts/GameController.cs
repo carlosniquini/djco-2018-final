7b398cb78d918364fbfdf8fef6931c39751ca2a9
Carlos Niquini <niquini.carlos@gmail.com>
2018-05-27 02:13:23 +0100

    Sons, 3Ds e Post-processing

--- unity-project/Assets/Scripts/GameController.cs
+++ unity-project/Assets/Scripts/GameController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.PostProcessing;
 
 public class GameController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  private Player player;
+  public AudioSource[] lakeSounds;
+
+
+  // Use this for initialization
+  void Start () {
+    player = GameObject.Find("FPSController").GetComponent<Player>();
+  }
+
+  void SwitchLakeSounds() {
+
+  }
+
+  // Update is called once per frame
+  void Update () {
+    //SwitchLakeSounds();
+  }
 }
--- unity-project/Assets/Scripts/Player.cs
+++ unity-project/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.PostProcessing;
 using UnityStandardAssets.Characters.FirstPerson;
 
@@ -14,16 +15,19 @@ public class Player : MonoBehaviour {
   private AudioSource audioSourceSteps;
   private AudioSource audioSourceAmbiente;
   private bool underlake = false;
+  private bool walke_lake = false;
   private TaskDefault task_1;
   private TerrainController terrainController;
-
-  public AudioClip lake;
+  private int current_idx;
+  private Image panelIntro;
+  public AudioClip ambiente_underlake;
   public AudioClip ambiente;
 
   public AudioClip[] fstp_grass;
   public AudioClip[] fstp_sand;
   public AudioClip[] fstp_dry_leaves;
   public AudioClip[] fstp_concrete;
+  public AudioClip[] fstp_water;
 
   private void Awake() {
     //Options.SetOptios(GameObject.Find("Options").GetComponent<Animator>());
@@ -32,6 +36,7 @@ public class Player : MonoBehaviour {
   // Use this for initialization
   void Start () {
     fpsController = this.GetComponent<FirstPersonController>();
+    panelIntro = GameObject.Find("PanelIntro").GetComponent<Image>();
     inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
     options = GameObject.Find("Options").GetComponent<Animator>();
     ppp = this.GetComponentInChildren<PostProcessingBehaviour>().profile;
@@ -40,29 +45,61 @@ public class Player : MonoBehaviour {
     task_1 = GameObject.Find("Task1").GetComponent<TaskDefault>();
     terrainController = GameObject.Find("Terrain").GetComponent<TerrainController>();
     ChangeFoostepsSound(0);
+    StartCoroutine(Wake());
     //TerrainController.changeEvent += ChangeFoostepsSound;
     //var aux = ppp.depthOfField.settings;
     //aux.focusDistance = 5f;
     //ppp.depthOfField.settings = aux;
   }
 
+  IEnumerator Wake() {
+    MuteAudioSteps(true);
+    float alpha = 1;
+    panelIntro.color = new Color(panelIntro.color.r, panelIntro.color.g, panelIntro.color.b, alpha);
+    var aux = ppp.vignette.settings;
+    aux.intensity = 1f;
+    ppp.vignette.settings = aux;
+    var aux_2 = ppp.colorGrading.settings;
+    aux_2.basic.saturation = 1f;
+    ppp.colorGrading.settings = aux_2;
+    yield return new WaitForSeconds(10f);
+    MuteAudioSteps(false);
+    while (aux.intensity >= 0) {
+      aux.intensity -= 0.01f;
+      ppp.vignette.settings = aux;
+      aux_2.basic.saturation -= 0.01f;
+      ppp.colorGrading.settings = aux_2;
+      panelIntro.color = new Color(panelIntro.color.r, panelIntro.color.g, panelIntro.color.b, alpha -= 0.2f * Time.deltaTime);
+      yield return new WaitForSeconds(0.1f);
+    }
+  }
+
   public void ChangeFoostepsSound(int idx) {
-    Debug.Log("Texture_id: " + idx);
-    if (idx == 0)
+    //Debug.Log("Texture_id: " + idx);
+    current_idx = idx;
+    if (walke_lake) {
+      fpsController.FootstepsSounds = fstp_water;
+      return;
+    }
+    if (idx == 0) {
       fpsController.FootstepsSounds = fstp_grass;
-    if (idx == 1)
+    }
+    if (idx == 1) {
       fpsController.FootstepsSounds = fstp_sand;
-    if (idx == 2)
+    }
+    if (idx == 2) {
       fpsController.FootstepsSounds = fstp_dry_leaves;
-    if (idx == 3)
+    }
+    if (idx == 3) {
       fpsController.FootstepsSounds = fstp_concrete;
+    }
   }
 
   // Update is called once per frame
   void Update () {
 
     //terrainController.GetMainTexture(this.transform.position);
-
+    UpdateAmbienteSound();
     if (Input.GetKeyDown("i")) {
       inventory.ShowInventory();
     }
@@ -91,16 +128,36 @@ public class Player : MonoBehaviour {
 
   private void OnTriggerExit(Collider other) {
     if (other.gameObject.tag == "lake" && gameObject.transform.position.y < other.gameObject.transform.position.y) {
-      audioSourceAmbiente.clip = lake;
+      audioSourceAmbiente.clip = ambiente_underlake;
       audioSourceSteps.mute = true;
       audioSourceAmbiente.Play();
       UnderLake = true;
     }
-    if (other.gameObject.tag == "lake" && gameObject.transform.position.y > other.gameObject.transform.position.y) {
+    if (other.gameObject.tag == "lake" && gameObject.transform.position.y >= other.gameObject.transform.position.y) {
       audioSourceAmbiente.clip = ambiente;
       audioSourceSteps.mute = false;
       audioSourceAmbiente.Play();
       UnderLake = false;
+      walke_lake = false;
+      ChangeFoostepsSound(current_idx);
     }
+  }
+
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject.tag == "lake" && gameObject.transform.position.y > other.gameObject.transform.position.y) {
+      walke_lake = true;
+      ChangeFoostepsSound(current_idx);
+    }
+  }
+
+  void UpdateAmbienteSound() {
+    if (UnderLake && gameObject.transform.position.y >= GameObject.Find("WaterBasicDaytime").transform.position.y) {
+      audioSourceAmbiente.clip = ambiente;
+      UnderLake = false;
+    }
+  }
+
+  public void MuteAudioSteps(bool s) {
+    this.GetComponent<AudioSource>().mute = s;
   }
 }
